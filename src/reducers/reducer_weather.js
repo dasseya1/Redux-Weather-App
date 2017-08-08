aff0d829b1efd894fa878bea1137f2d9e4b9c2ff
Damien <dasseya1@gmail.com>
2017-08-07 21:51:18 -0400

    Created weather reducer and added it to CombineReducers

---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,10 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state=[], action) {
+    switch(action.type) {
+        case FETCH_WEATHER: 
+            //return state.concat([action.payload.data]); ***ES6 syntax to write the same code below***
+            return [action.payload.data, ...state]; // [city, city, city]
+    }
+    return state;
+}
