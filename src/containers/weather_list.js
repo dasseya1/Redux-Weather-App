22b78568b79565f1480b9f7a064fd876003c7d84
Damien <dasseya1@gmail.com>
2017-08-09 20:53:14 -0400

    More styling and google maps integration

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_maps';
 import _ from 'lodash';
 
 class WeatherList extends Component {
@@ -10,10 +11,11 @@ class WeatherList extends Component {
         const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lon, lat } = cityData.city.coord;
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td><Chart data={temps} color="orange" units="°C" /></td>
                 <td><Chart data={pressures} color="green" units="hPA" /></td>
                 <td><Chart data={humidities} color="black" units="%" /></td>
